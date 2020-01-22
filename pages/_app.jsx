1e68ec41c71acb9ac01be1d294ee53df079f7d88
Dika <dika@Dikas-Macbook.local>
2020-01-22 15:32:22 +0700

    Update Readme and Set page title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import App from "next/app";
 import Router from "next/router";
 import NProgress from "nprogress";
 import React from "react";
+import Head from "next/head";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -13,6 +14,9 @@ class MyApp extends App {
 
         return (
             <>
+                <Head>
+                    <title>Todo Example</title>
+                </Head>
                 <Component { ...pageProps } key={ router.route }/>
             </>
         );
